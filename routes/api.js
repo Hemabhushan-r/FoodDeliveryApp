75e515e009ca795b176e46eab7835461299eb6bd
Hemabhushan-r <hemabhushanr3@gmail.com>
2022-10-03 23:51:03 +0530

    Initial Requisites-1

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+router.get('/data', (req, res, next) => {
+  // get placeholder
+});
+router.post('/data', (req, res, next) => {
+  // post placeholder
+});
+router.delete('/data/:id', (req, res, next) => {
+  // delete placeholder
+});
+module.exports = router;
